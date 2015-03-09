7eb48c62f56f241db34fd3eb1923ef900de5de95
Sam Hatoum <sam@hatoum.net>
2015-03-09 00:13:37 -0700

    Update test-app.js

--- example/test-app.js
+++ example/test-app.js
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
   Session.setDefault('enterCounter', 0);
-  Session.setDefault('keypressCounter', 0);
+
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
